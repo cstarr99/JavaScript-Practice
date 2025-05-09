cd96d0cfae9abbdd7367184fa44122fb2c5e478f
Chase Starr <chasestarr9@gmail.com>
2025-05-08 20:06:48 -0400

    practiced functions

---
+++ FunctionPractice/script.js
@@ -0,0 +1,51 @@
+//function that prints my name.
+function printMyName() {
+  console.log("Chase Starr");
+}
+
+//calling printMyName function.
+printMyName();
+
+/* function that prints the result of subtracting
+ first argument - second argument. */
+function subtract(a, b) {
+  console.log(a - b);
+}
+
+let x = 4;
+let y = 2;
+
+//calling subtract function
+subtract(x, y);
+
+//function that returns sum of a + b
+function sum(a, b) {
+  return a + b;
+}
+
+let s = sum(2, 4);
+console.log(s);
+
+let z = sum(s, 2);
+console.log(z);
+
+//function that takes in a name and then returns hello + name.
+function hello(name) {
+  return "Hello " + name;
+}
+
+console.log(hello("Chase"));
+
+//callback function.
+
+//function that takes in variable and prints that variable.
+function printVariable(Variable) {
+  console.log(Variable);
+}
+
+//function that takes in function as x. the function then uses "hello world" as argument in print variable function.
+function func(x) {
+  x("hello world");
+}
+
+func(printVariable);
