bd5ba33cc73c9d203cffdcd4d45488b57c9117ba
Chase Starr <chasestarr9@gmail.com>
2025-05-16 11:14:22 -0400

    added array methods to array practice file.

--- ArrayPractice/script.js
+++ ArrayPractice/script.js
@@ -44,3 +44,88 @@ console.log(d[2][1]);
 //How to get the length of an array.
 
 console.log(d.length);
+
+/*
+ *
+ *
+ */
+
+//ARRAY METHODS
+
+const array = [1, 2, 3, 4, 5];
+
+//The forEach method goes through each of the items in the array.
+
+array.forEach((number, index) => {
+  console.log(number + " " + index);
+});
+
+//The Map method loops through the array, changes it in some way, and returns a new different array.
+
+const mapArray = array.map((number) => {
+  return number * 2;
+});
+
+console.log(mapArray);
+
+//The filter method filters down to only select values, and then returns new array.
+
+const filterArray = array.filter((number) => {
+  return number > 3;
+});
+
+console.log(filterArray);
+
+//The find method will find return the first element that matches the parameters.
+
+const findArray = array.find((number) => {
+  return number > 4;
+});
+
+console.log(findArray);
+
+//The some method will see if at least one of the elements in array meet the parameters, and then return a boolean.
+
+const isTrue = array.some((number) => {
+  return number > 5;
+});
+
+console.log(isTrue);
+
+//The every method will see if every element in the array matches the parameters, and returns a boolean.
+
+const everyArray = array.every((number) => {
+  return number > 0;
+});
+
+console.log(everyArray);
+
+//The reduce method goes through the array and reduces it to only one element.
+
+const sum = array.reduce((sum, number) => {
+  return sum + number;
+}, 0);
+
+console.log(sum);
+
+//Reduce method using an array of objects;
+
+const items = [
+  { price: 10 },
+  { price: 20 },
+  { price: 14 },
+  { price: 1 },
+  { price: 6 },
+];
+
+const sumItems = items.reduce((sum, item) => {
+  return sum + item.price;
+}, 0);
+
+console.log(sumItems);
+
+//The include method just checks to see if the array includes that element and returns a boolean.
+
+const includesArray = array.includes(4);
+
+console.log(includesArray);
