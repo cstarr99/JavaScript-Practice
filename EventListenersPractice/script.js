d171c463e817f98609538bd9eb59bd36ddbde780
Chase Starr <chasestarr9@gmail.com>
2025-05-17 13:22:32 -0400

    made file to practice event listeners.

---
+++ EventListenersPractice/script.js
@@ -0,0 +1,41 @@
+//An event listener will allow you to change elements from html.
+
+const button = document.querySelector("[data-btn]");
+
+//when you click the button then the console will print "clicked".
+button.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+/*
+ *
+ *
+ *
+ */
+
+//remove event listener by calling the function.
+
+//function that prints "remove" to console.
+function clicked() {
+  console.log("remove");
+}
+
+//when button is clicked it runs the function that prints "remove"
+button.addEventListener("click", clicked());
+
+//when button is clicked it removes the clicked function and thus doesn't print "remove"
+button.removeEventListener("click", clicked());
+
+/*
+ *
+ *
+ *
+ */
+
+//when link is clicked, prevent default and log something to console.
+const link = document.querySelector(".text");
+
+link.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicked link");
+});
