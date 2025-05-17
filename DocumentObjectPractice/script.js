4f1317e2cacfd7b20ce9bdca561e849c04239bac
Chase Starr <chasestarr9@gmail.com>
2025-05-17 12:29:28 -0400

    made a file to practice the document object.

---
+++ DocumentObjectPractice/script.js
@@ -0,0 +1,9 @@
+//The Document object allows you to access everything in HTML.
+//For example you can add to your HTML.
+
+//Creating the element.
+const element = document.createElement("h1");
+//Adding text to the element.
+element.innerText = "Hello World";
+//Adding the element to the body.
+document.body.appendChild(element);
