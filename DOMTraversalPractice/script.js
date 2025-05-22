c22f39fe31c3a4284c0ec2c1a60bbd913e2ea139
Chase Starr <chasestarr9@gmail.com>
2025-05-22 08:40:33 -0400

    made file to practice dom traversal

---
+++ DOMTraversalPractice/script.js
@@ -0,0 +1,13 @@
+//Shows the different way you can connect elements from the DOM in JS.
+const grandparent = document.querySelector("#grandparent");
+
+const parentOne = childOne.parentElement;
+
+const parentTwo = childOne.nextElementSibling;
+
+const childOne = childTwo.previousElementSibling;
+
+const childTwo = parentOne.children[1];
+
+//closest looks at all parents of chosen element and find very first one that matches description.
+const greatGrandparent = childTwo.closest("Great-grandparent");
