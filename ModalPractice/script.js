b85e4089e2b14ac7b861677fb40ef529ea35d3f8
Chase Starr <chasestarr9@gmail.com>
2025-05-26 07:49:19 -0400

    made code for modal practice more concise.

--- ModalPractice/script.js
+++ ModalPractice/script.js
@@ -4,22 +4,20 @@ const closeModalBtn = document.querySelector("#close-modal-btn");
 const overlay = document.querySelector("#overlay");
 
 //Create a click event listener for the open-modal-btn that adds the class "open" to the modal
-openModalBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+openModalBtn.addEventListener("click", () => {
   modal.classList.add("open");
   //Also add the class "open" to the overlay
   overlay.classList.add("open");
 });
 
 // Create a click event listener for the close-modal-btn that removes the class "open" from the modal
-closeModalBtn.addEventListener("click", () => {
-  modal.classList.remove("open");
-  //remove the class "open" from the overlay
-  overlay.classList.remove("open");
-});
+closeModalBtn.addEventListener("click", closeModal);
 
 //Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener("click", () => {
+overlay.addEventListener("click", closeModal);
+
+//function that will take class of "open" out of overlay and modal, which closes modal.
+function closeModal() {
   overlay.classList.remove("open");
   modal.classList.remove("open");
-});
+}
