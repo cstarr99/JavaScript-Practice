93bd7ac7b09ada96f96a6e1da630ec6f008310e1
Chase Starr <chasestarr9@gmail.com>
2025-05-27 09:20:13 -0400

    created a file to practice while loops.

---
+++ WhileLoopPractice/script.js
@@ -0,0 +1,8 @@
+//While loops work but continuing the inside code as long as the given statement is still true.
+//While loops are useful when you DON'T KNOW how many times you need to loop.
+let counter = 0;
+
+while (counter < 5) {
+  console.log(counter);
+  counter++;
+}
