d7faf8454d6443e9665441bfd34b1f6f70b83fe2
Chase Starr <chasestarr9@gmail.com>
2025-06-02 22:43:22 -0400

    finished making quiz app.

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,36 +1,39 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
-
 const form = document.querySelector("#quiz-form");
 const answers = Array.from(document.querySelectorAll(".answer"));
 const alert = document.querySelector("#alert");
 const questions = Array.from(document.querySelectorAll(".question-item"));
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behavior
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  //finds all selected answers out of all answer choices.
   selectedAnswers = answers.filter((answers) => answers.checked);
+  questions.forEach((question) => {
+    question.classList.add("incorrect");
+    question.classList.remove("correct");
+  });
   selectedAnswers.forEach((answer) => {
+    //variable that sees is the answer is right
     const isTrue = answer.value === "true";
+    //finds the answers closest question item parent.
     const questionItem = answer.closest(".question-item");
+    //if the questions value is true it will make it correct, and if it is wrong it will make it incorrect.
     if (isTrue) {
       questionItem.classList.add("correct");
       questionItem.classList.remove("incorrect");
     } else {
       questionItem.classList.remove("correct");
       questionItem.classList.add("incorrect");
+    }
+    //variable that sees if every selected is answer is true
+    const allTrue = selectedAnswers.every((answer) => answer.value === "true");
+    //variable that see if every question is answered.
+    const allAnswered = selectedAnswers.length === questions.length;
+    //if statement that makes congratulations tab pop up and leave after 1 second.
+    if (allTrue && allAnswered) {
+      alert.classList.add("active");
+      setTimeout(() => {
+        alert.classList.remove("active");
+      }, 1000);
     }
   });
 });
