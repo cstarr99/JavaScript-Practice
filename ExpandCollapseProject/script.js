1376a7ff5c3d6dbca05aeb9bef4f6eb773ba16a4
Chase Starr <chasestarr9@gmail.com>
2025-06-18 16:53:00 -0400

    Made a project to practice using expand/collapse bars.

--- ExpandCollapseProject/script.js
+++ ExpandCollapseProject/script.js
@@ -1,14 +1,32 @@
-const button = document.querySelector(".expand-button");
-const cardBody = document.querySelector(".card-body");
-let cardHeader = Array.from(document.querySelectorAll(".card-header"));
-let buttonArray = [];
+document.addEventListener("click", (e) => {
+  const currentButton = e.target;
+  const cardHeader = e.target.closest(".card-header");
+  const cardBody = cardHeader.nextElementSibling;
 
-button.addEventListener("click", () => {
-  if (button.innerText === "Expand") {
+  if (e.target.matches("button") && currentButton.innerText === "Expand") {
     cardBody.classList.add("show");
-    button.innerText = "Collapse";
-  } else if (button.innerText === "Collapse") {
+    currentButton.innerText = "Collapse";
+  } else if (
+    e.target.matches("button") &&
+    currentButton.innerText === "Collapse"
+  ) {
     cardBody.classList.remove("show");
-    button.innerText = "Expand";
+    currentButton.innerText = "Expand";
   }
 });
+
+//SECOND WAY TO DO IT.
+// document.addEventListener("click", e => {
+//     if (!e.target.matches(".expand-button")) return
+
+//     const card = e.target.closest(".card")
+//     const cardBody = card.querySelector(".card-body")
+
+//     cardBody.classList.toggle("show")
+//     if (e.target.innerText === "Expand") {
+//       e.target.innerText = "Collapse"
+//     } else {
+//       e.target.innerText = "Expand"
+//     }
+//     // e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand"
+//   })
