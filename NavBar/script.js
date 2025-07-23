98b1777a6142a2a60f7ccdbda5dfdcce2e6abe2a
Chase Starr <chasestarr9@gmail.com>
2025-07-22 23:09:06 -0400

    finished nav bar

--- NavBar/script.js
+++ NavBar/script.js
@@ -1,13 +1,17 @@
 const countryList = document.querySelector(".country-list");
 
-// document.addEventListener("mouseover", (e) => {
-//   e.preventDefault();
-//   if (!e.target.matches(".nav-item")) return;
-//   countryList.style.display = "block";
-// });
+document.addEventListener("mouseover", (e) => {
+  e.preventDefault();
+  if (!e.target.matches(".nav-item")) return;
+  const currentButton = e.target;
+  const closestCountryList = currentButton.children[0];
+  closestCountryList.style.display = "block";
+});
 
-// document.addEventListener("mouseout", (e) => {
-//   e.preventDefault();
-//   if (!e.target.matches(".nav-item")) return;
-//   countryList.style.display = "none";
-// });
+document.addEventListener("mouseout", (e) => {
+  e.preventDefault();
+  if (!e.target.matches(".nav-item")) return;
+  const currentButton = e.target;
+  const closestCountryList = currentButton.children[0];
+  closestCountryList.style.display = "none";
+});
