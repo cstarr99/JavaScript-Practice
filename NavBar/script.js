c2599e100ca0b9b53b381d9d56663e469473aed9
Chase Starr <chasestarr9@gmail.com>
2025-07-22 21:40:06 -0400

    added js to allow tabs to appear/disappear on mouseover

---
+++ NavBar/script.js
@@ -0,0 +1,13 @@
+const countryList = document.querySelector(".country-list");
+
+// document.addEventListener("mouseover", (e) => {
+//   e.preventDefault();
+//   if (!e.target.matches(".nav-item")) return;
+//   countryList.style.display = "block";
+// });
+
+// document.addEventListener("mouseout", (e) => {
+//   e.preventDefault();
+//   if (!e.target.matches(".nav-item")) return;
+//   countryList.style.display = "none";
+// });
