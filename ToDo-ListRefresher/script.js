ab1584965fb392e7c8ecc276de6c938171a59362
Chase Starr <chasestarr9@gmail.com>
2025-11-15 12:49:39 -0500

    Finished todo refresher

--- ToDo-ListRefresher/script.js
+++ ToDo-ListRefresher/script.js
@@ -1,18 +1,17 @@
-const item = document.querySelector("#input");
+const input = document.querySelector("#input");
 const form = document.querySelector(".add-item");
 const incompleteList = document.querySelector(".incomplete-items");
 const completeList = document.querySelector(".complete-items");
 
-form.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const div = document.createElement("div");
-  div.innerText = item.value;
+  div.innerText = input.value;
   div.classList.add("list-item");
   incompleteList.appendChild(div);
-  console.log(div);
-});
 
-item.addEventListener("click", () => {
-  incompleteList.removeChild(item);
-  completeList.appendChild(item);
+  div.addEventListener("click", () => {
+    incompleteList.removeChild(div);
+    completeList.appendChild(div);
+  });
 });
